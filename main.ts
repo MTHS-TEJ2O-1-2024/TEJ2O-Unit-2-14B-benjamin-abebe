ba55693b053f63d1829541a8e0092a4bd3af1be9
benjamin-abebe <benjamin.abebe@mths.ca>
2024-11-04 17:31:51 -0500

    Made change on the typescript version.

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
+ * Created by: Benjamin Abebe
+ * Created on: Oct 2024
  * This program shows while loops in action
 */
 
@@ -55,6 +55,44 @@ input.onButtonPressed(Button.A, function () {
         loopCounter = loopCounter - 1
     }
     
+    sprite.delete()
+    basic.showIcon(IconNames.Happy)
+})
+
+// When "B" is pressed
+input.onButtonPressed(Button.B, function () {
+    // setup
+    basic.clearScreen()
+    loopCounter = 0
+    sprite = game.createSprite(0, 0)
+
+    loopCounter = 0
+    while (loopCounter <= 5) {
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+        loopCounter = loopCounter + 1
+        pause(500)
+    }
+
+    loopCounter = 0
+    while (loopCounter <= 5) {
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        loopCounter = loopCounter + 1
+        pause(500)
+    }
+
+    loopCounter = 5
+    while (loopCounter >= 0) {
+        sprite.set(LedSpriteProperty.Y, loopCounter)
+        loopCounter = loopCounter - 1
+        pause(500)
+    }
+
+    loopCounter = 5
+    while (loopCounter >= 0) {
+        sprite.set(LedSpriteProperty.X, loopCounter)
+        loopCounter = loopCounter - 1
+        pause(500)
+    }
     sprite.delete()
     basic.showIcon(IconNames.Happy)
 })
